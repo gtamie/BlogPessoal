614a17e2bf940f8b188f754660a3d03dfc2e7e83
Giuliana <giulianatamie@gmail.com>
2022-02-22 15:50:22 -0300

    Validação login e cadastrar

--- angular/projeto/blogPessoal/src/app/cadastrar/cadastrar.component.ts
+++ angular/projeto/blogPessoal/src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
+
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario
+  confirmarSenha: string
+  tipoUser: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {   }
+
+  ngOnInit() {
+    window.scroll(0,0)
   }
+
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUsuario(event: any) {
+    this.tipoUser = event.target.value
+  }
+
+  cadastrar(){
+    this.usuario.tipo = this.tipoUser
+    
+    if (this.usuario.senha!=this.confirmarSenha){
+      alert("As senhas estão diferentes.")
+    }else{
+      this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+        this.usuario=resp
+        this.router.navigate(['/entrar'])
+        alert("Usuário cadastrado com sucesso!")
+      })
+    }
+  }
+  
 
 }
