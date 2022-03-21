b5004d016f56233cf82339b0a4e094646348f11f
Giuliana Tamie Sumida <89866151+gtamie@users.noreply.github.com>
2022-03-21 14:49:09 -0300

    Delete angular/projeto/blogPessoal directory

--- angular/projeto/blogPessoal/src/app/model/UsuarioLogin.ts
+++
@@ -1,9 +0,0 @@
-export class UsuarioLogin {
-    public id: number
-    public nome: string
-    public usuario: string
-    public senha: string
-    public foto: string
-    public tipo: string
-    public token: string
-}
